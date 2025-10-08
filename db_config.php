b4f644e534edf85dc9d4ebdeb7516b08cc88cda5
Shane-La <lklathu1210@gmail.com>
2025-10-08 15:34:22 +0530

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -1,12 +1,12 @@
 <?php
-// Database configuration
+
 $db_host = "localhost";
 $db_user = "root";
 $db_pass = "";
 $port = "3308";
 $db_name = "boarding_gigs";
 
-// Connect to database using mysqli_connect
+
 $connection = mysqli_connect($db_host, $db_user, $db_pass, $db_name, $port);
 
 if (!$connection) {
